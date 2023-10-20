d4c52bc68d3696855bfed92c14e2bf1b35fe1797
tiinym81 <148569045+tiinym81@users.noreply.github.com>
2023-10-20 10:38:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_nqb0lsp", "template_f7hle4i", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
